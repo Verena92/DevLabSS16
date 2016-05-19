7c12191b6371ca0dc1a17a040e5d9d797635189e
bm066 <bm066@hdm-stuttgart.de>
2016-05-19 15:24:26 +0200

    firstName und lastName lassen sich getrennt abfragen.

--- SpeechTokenizer/war/user/user.js
+++ SpeechTokenizer/war/user/user.js
@@ -10,13 +10,20 @@ function getUserData(userData){
 	var localParticipant=gapi.hangout.getLocalParticipant();
 	
 	if(userData){
-		if(userData=='name'){
-			data=JSON.stringify(localParticipant.person.displayName);
+		if(userData=='firstName'||userData=='lastName'){
+			var name=JSON.stringify(localParticipant.person.displayName);
+			name=name.replace(/\"/g,'');
+			var nameArray=name.split(/\s+/);
+			if(userData=='firstName'){
+				data=nameArray[0];
+			}else if(userData=='lastName'){
+				data=nameArray[1];
+			}
 		}else{
 			data=JSON.stringify(localParticipant[userData]);		
 		}
 	}else{
-		console.log('Falsches Attribut! Siehe: https://developers.google.com/+/hangouts/api/gapi.hangout.html#classes-in-gapihangout');
+		console.log('Falsches Argument in getUserData! Siehe: https://developers.google.com/+/hangouts/api/gapi.hangout.html#classes-in-gapihangout');
 	}
 	
 	return data;
