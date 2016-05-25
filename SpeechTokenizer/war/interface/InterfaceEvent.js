a4ebc0c1d4be50a1679639c0c1880a5b94a6a182
monabrunner <monabrunner@10.60.43.230>
2016-05-25 18:14:10 +0200

    Interface zu Event

---
+++ SpeechTokenizer/war/interface/InterfaceEvent.js
@@ -0,0 +1,24 @@
+/**
+ * Autor Mona
+ */
+
+$.ajax({ method: "POST", url: "IP-Adresse-zur-Schnittstelle/nameSchnittstelle", data: { name: "John", location: "Boston" }
+
+    error: function( ){
+
+        console.log("Error");
+
+    },
+
+    success: function( response_data ){
+
+        // hier response_data 
+    
+   
+
+    	        new_keys = response_data["new_keys"]
+
+    	}
+
+});
+
