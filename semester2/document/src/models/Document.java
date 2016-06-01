0b1f299bd7063bce922faa360ee79d4eac229943
jaufmann <wjaufmann@gmx.de>
2016-06-01 17:02:13 +0200

    added document metadata service program
    Added speech token interface, and google apps script interface for edit
    and change document

---
+++ semester2/document/src/models/Document.java
@@ -0,0 +1,63 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Document {
+	private String name;
+	private String type;
+	private String creationDate;
+	private String path;
+	private String createdBy;
+	private String status;
+	private double version;
+	private ArrayList<String> listKeyword;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(String creationDate) {
+		this.creationDate = creationDate;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public ArrayList<String> getListKeyword() {
+		return listKeyword;
+	}
+	public void setListKeyword(ArrayList<String> listKeyword) {
+		this.listKeyword = listKeyword;
+	}
+	public double getVersion() {
+		return version;
+	}
+	public void setVersion(double version) {
+		this.version = version;
+	}
+}
