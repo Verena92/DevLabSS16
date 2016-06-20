2e181c6cbb70078a9275bbb55691dce1497d0ea4
eclipsebennoem <eclipsebennoem@10.60.34.152>
2016-06-20 15:22:13 +0200

    local version is working, now trying app engine

--- SpeechTokenizer/war/documentResults/displayResults.js
+++ SpeechTokenizer/war/documentResults/displayResults.js
@@ -2,19 +2,24 @@
  * Autor: Benjamin Mateja
  */
 
+var displayText = "<ul>";
+
 function GetDocuments(){
 	
 	//create documentarray for documents
 	var DocumentArray = new Array();
 	
+	
+	var i;
+	
 	//create first samle document
-	var Document = new Object();
-	Document.id = 1;
-	Document.UserId = "123@456";
-	Document.hangoutsId = "abcdef";
-	Document.title = "BMW Projektbericht 2016";
-	Document.path = "www.google.de";
-	Document.show = title.link(path);
+	var Document1 = new Object();
+	Document1.id = 1;
+	Document1.UserId = "123@456";
+	Document1.hangoutsId = "abcdef";
+	Document1.title = "BMW Projektbericht 2016";
+	Document1.path = "www.google.de";
+	Document1.show = Document1.title.link(Document1.path);
 	
 	//create second sample document
 	var Document2 = new Object();
@@ -23,18 +28,23 @@ function GetDocuments(){
 	Document2.hangoutsId = "abcdef";
 	Document2.title = "Mercedes Projektbericht 2016";
 	Document2.path = "www.spox.com";
-	Document2.show = title.link(path);
+	Document2.show = Document2.title.link(Document2.path);
 	
 	/**
 	 *code here: if document.hangoutsid = user.hangoutsid then push into document array, else not
 	 */
 	
 	//push document in documentarray
-	DocumentArray.push(Document);
+	DocumentArray.push(Document1);
 	DocumentArray.push(Document2);
 	
-	var Results = document.show;
+
 	
-	console.log(DocumentArray);
+	for (i = 0; i < DocumentArray.length; i++){
+		displayText += "<li>" + DocumentArray[i].show + "</li>";
+	}
+	displayText += "</ul>";
+	
+	
 	
 }
