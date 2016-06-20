3af6a0cd4d28d781129d07a8a33262597860dc83
eclipsebennoem <eclipsebennoem@10.60.34.152>
2016-06-20 12:42:35 +0200

    implemented first try for displaying of documentresults

---
+++ SpeechTokenizer/war/documentResults/displayResults.js
@@ -0,0 +1,38 @@
+/**
+ * Autor: Benjamin Mateja
+ */
+
+function GetDocuments(){
+	
+	//create documentarray for documents
+	var DocumentArray = new Array();
+	
+	//create first samle document
+	var Document = new Object();
+	Document.id = 1;
+	Document.UserId = "123@456";
+	Document.hangoutsId = "abcdef";
+	Document.title = "BMW Projektbericht 2016";
+	Document.path = "www.google.de";
+	Document.show = title.link(path);
+	
+	//create second sample document
+	var Document2 = new Object();
+	Document2.id = 2;
+	Document2.UserId = "123@456";
+	Document2.hangoutsId = "abcdef";
+	Document2.title = "Mercedes Projektbericht 2016";
+	Document2.path = "www.spox.com";
+	Document2.show = title.link(path);
+	
+	/**
+	 *code here: if document.hangoutsid = user.hangoutsid then push into document array, else not
+	 */
+	
+	//push document in documentarray
+	DocumentArray.push(Document);
+	DocumentArray.push(Document2);
+	
+	console.log(DocumentArray);
+	
+}
