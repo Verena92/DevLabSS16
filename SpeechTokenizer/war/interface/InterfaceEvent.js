833eed230135abc95f0e49df0515388bd73560d1
monabrunner <monabrunner@10.60.44.130>
2016-06-22 13:47:11 +0200

    Update Interface Event

--- SpeechTokenizer/war/interface/InterfaceEvent.js
+++ SpeechTokenizer/war/interface/InterfaceEvent.js
@@ -30,20 +30,5 @@ $.ajax({ type:'POST',
 }
 
 
-/*$.ajax({ method: "POST", url: "IP-Adresse-zur-Schnittstelle/nameSchnittstelle", data: { name: "John", location: "Boston" }
 
-    error: function( ){
-
-        console.log("Error");
-
-    },
-
-    success: function( response_data ){
-
-        // hier response_data 
-  
-
-    	        new_keys = response_data["new_keys"]
-    	}
-});*/
 
