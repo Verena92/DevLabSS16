604c275ccc94a32fa9d85cc378abd164eb2ada7b
verenahofmann <verenahofmann@10.60.46.128>
2016-06-28 14:15:13 +0200

    kommentiert Webspeech.js, JSON.js Delete testPostInterface

--- SpeechTokenizer/war/Webspeech.js
+++ SpeechTokenizer/war/Webspeech.js
@@ -1,5 +1,6 @@
 /**
- * Autor: Verena Hofmann
+ * In dieser Klasse wird der Google Webspeech Service aufgerufen, welche durch den StartButton aufgerufen wird.
+ * @author Verena Hofmann
  */
 
 
@@ -9,20 +10,34 @@ var ignore_onend;
 
 
 	if (!('webkitSpeechRecognition' in window)) { 
-			//Hier wird geprüft, ob der Browser die Web Speech API unterstützt. Falls nicht wird er aufgefordert den Browser upzudaten.
+		
+			/**Hier wird geprüft, ob der Browser die Web Speech API unterstützt. 
+			Falls nicht wird er mit Hilfe der Methode <code>update()</code> aufgefordert den Browser upzudaten.
+			*/
 			upgrade ();
+			/**
+			 * Falls nicht wird else ausgeführt
+			 */
+			
 		} else {
-			/*Erlaubt der Seite Zugriff auf das Mikrofon - webkitSpeechRecognition Objekt stellt das Speech Interface bereit und definiert Eventhandler*/
-			//That is the object that will manage our whole recognition process.
+			/**
+			 * Erlaubt der Seite Zugriff auf das Mikrofon - Das webkitSpeechRecognition() Objekt 
+			 * stellt das Google Webspeech Interface bereit und definiert Eventhandler.
+			 * Über das @param recognition wird der gesamte Recognition-Prozess verwaltet
+			 */
 			var recognition = new webkitSpeechRecognition();
+			
   			/* Continous: true = User kann Pause einlegen, bei false wird sofort geschickt sobald pause*/
   			recognition.continuous = true;
 	
-			//Listening (capturing voice from audio input) started.
+			/*Listening (capturing voice from audio input) started.*/
 			recognition.onstart = function() {
     			recognizing = true;
+    			
+    			/**Mit dieser Methode wird der Status des Mikrofons ausgelesen 
+    			 * und falls es aktiv ist grün "Mic aktiv" dargestellt. 
+    			 * */
     	  		var micstatuselement = document.getElementById('micstatus');
-    	  		
     	  		/**
     	  		 * Outcomment the next two lines for tests on local server.
     	  		 * You need the two lines just for App Engine.
@@ -30,12 +45,17 @@ var ignore_onend;
     	  		micstatuselement.style.color='green';
     	  		micstatuselement.innerHTML='Mic aktiv';
   			};
-  			  	
+  			
+  			/**Diese Funktion <code>onend()</code> wird aufgerufen, wenn die Aufnahme beendet wurde. 
+  			 * Dabei wird das Attribut @param recognizing auf false gesetzt.
+  			*/
   			recognition.onend = function() {
     			recognizing = false;
     			if (ignore_onend) {
       				return;
     			}
+    			
+    			/*Mit dieser Methode wird der Status des Mic ausgelesen und falls es aktiv "Mic inaktiv" ausgegeben.*/
     	  		var micstatuselement = document.getElementById('micstatus');
     	  		
     	  		/**
@@ -47,28 +67,37 @@ var ignore_onend;
 
   	};
   		
+  	/**
+  	 * Die Methode <code>onresult()</code> durchläuft mit einer For-Schleife des erzeugte Result des Audios. 
+  	 * @param sttResult wird anschließend durch <code>transcript</code> in einen Text umgewandelt.
+  	 * Durch den Aufruf der Methode <code>Token()</code> wird diese in der Klasse Token.js aufgerufen.
+  	 * Anschließend wird der Text in HTML übersetzt und in angezeigt.
+  	 */
   	
   	recognition.onresult = function(event) {
     			for (var i = event.resultIndex; i < event.results.length; ++i) {
       				if (event.results[i].isFinal) {
-      					//the text representation of a word.
         				sttResult = event.results[i][0].transcript;
       				}
       				
       				//Aufruf der Token Funktion in Token.js      				
       				Token();
-      				
     			}
     			/*linebreak für neuen Paragraphen, wird dann in HTML übersetzt*/
     			sttResult = capitalize(sttResult);
     			
     			/*final_span Anzeige in HTML*/
     			final_span.innerHTML = linebreak(sttResult);
-  			};
-  }
+  	};
+}
   				
   			
-/*Wird nach Drücken auf den Start-Button aufgerufen*/
+/**
+ * Die Methode <code>startButton(event)</code> wird nach Drücken des Start-Buttons aufgerufen.
+ * Dabei wird, falls die Aufnahme bereits läuft, diese zuerst gestoppt. Falls es gestartet wird,
+ * gibt die Console "Service gestartet" aus. Danach wird das @param sttResult geleert und die 
+ * durch die Methode <code>start()</code> 
+ */
 function startButton(event) {	
 	if (recognizing) {
     	recognition.stop();
@@ -81,65 +110,56 @@ function startButton(event) {
 	ignore_onend = false;
 	final_span.innerHTML = '';
 }
+
 	
 function refreshButton(event) {	
 	console.log("Service refreshed");
-
-
 	var Document = new Object();
 		Document.userId = "hangoutF2AA23F4_ephemeral.id.google.com^b005d30bf33378"
 		Document.hangoutsId = "AP36tYfMptB_3_cJW6AOwyJAIgAdMt5jKh6lyqVUTFVSapjVT0fRNg"
 	   	Document.documentName = "Endpoint"
 	   	Document.drivePath = "http://drive.google.com/open?id=1vJNvuPnCwg37yKZRsRuWvDn_LIwF5N4nHm_Xm1SIn8g"
 	   		
-	
-	
-//Asynchron Post Request to own interface for testing
-$.ajax({ type:'POST', 
-	url: 'https://146.148.67.230/de.hdm.speechtomcat/rest/PostDocuments',  
-	data: JSON.stringify(Document),
-	contentType: 'application/json',
-	datatyp: 'json',
-
-	//Fehler loggen 
-    error: function(){
-        console.log("Error");
-    },
-
-	//Bei erfolgreichem Request Objekt in der Console ausgeben
-    success: function(responsedata){
-    	console.log("success");
-    	console.log(Document);
-    		
+	//Asynchron Post Request to own interface for testing
+	$.ajax({ type:'POST', 
+		url: 'https://146.148.67.230/de.hdm.speechtomcat/rest/PostDocuments',  
+		data: JSON.stringify(Document),
+		contentType: 'application/json',
+		datatyp: 'json',
+		//Fehler loggen 
+		error: function(){
+			console.log("Error");
+		},
+		//Bei erfolgreichem Request Objekt in der Console ausgeben
+		success: function(responsedata){
+			console.log("success");
+    		console.log(Document);
     	}
-});
+	});
 }
 
 function ipButton(event) {	
 	console.log("Service gibt ID");
-
-	   		
-	
-//Asynchron Post Request to own interface for testing
-$.ajax({ type:'GET', 
-	url: 'https://146.148.67.230/de.hdm.speechtomcat/rest/register',  
-	contentType: 'application/x-www-form-urlencoded',
-	datatyp: 'text',
-
-	//Fehler loggen 
-    error: function(){
-        console.log("Error");
-    },
-
-	//Bei erfolgreichem Request Objekt in der Console ausgeben
-    success: function(responsedata){
-    	console.log("success");
-    	console.log(responsedata);
-    		
-    	}
-});
+	//Asynchron Post Request to own interface for testing
+	$.ajax({ type:'GET', 
+		url: 'https://146.148.67.230/de.hdm.speechtomcat/rest/register',  
+		contentType: 'application/x-www-form-urlencoded',
+		datatyp: 'text',
+		//Fehler loggen 
+		error: function(){
+			console.log("Error");
+		},
+		//Bei erfolgreichem Request Objekt in der Console ausgeben
+		success: function(responsedata){
+			console.log("success");
+			console.log(responsedata);
+    		}
+	});
 }
-/* Durch linebreak und capatilize wird Text in Box eingeblendet*/
+
+/** Durch die Methoden <code>linebreak(sttResult)</code> und <code>capatilize(sttResult)</code> wird Text, 
+ * also das sttResult in der App eingeblendet.
+ */
 
 function linebreak(sttResult) {
 	return sttResult;
--- SpeechTokenizer/war/tokenization/JSON.js
+++ SpeechTokenizer/war/tokenization/JSON.js
@@ -1,13 +1,10 @@
 /**
- * Autor: Benjamin Mateja
+ * Diese Klasse erzeugt aus dem @param TokenNew einen JSON mit Hilfe der Methode
+ * <code>JSON.stringify(TokenNew)</code>.
+ * @author Benjamin Mateja
  */
 
 function Stringify (TokenNew){
-	
 	var jsonString = JSON.stringify(TokenNew);
-	
 	console.log(jsonString);
-	
-	
-	
 }
